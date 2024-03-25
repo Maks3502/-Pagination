95ef95fc8dc845a85d31ab6c843835cdb8069d62
Maks3502 <107855225+Maks3502@users.noreply.github.com>
2024-03-25 22:05:14 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const apiKey = '43066130-5334719edca1429b8c556a89d';
+const perPage = 10;
+let currentPage = 1;
+
+const imageContainer = document.getElementById('image-container');
+const loadMoreButton = document.getElementById('load-more');
+
+async function fetchImages() {
+    const response = await fetch(`https://pixabay.com/api/?key=${apiKey}&editors_choice=true&page=${currentPage}&per_page=${perPage}`);
+    const data = await response.json();
+    return data.hits;
+}
+
+async function displayImages() {
+    const images = await fetchImages();
+    images.forEach(image => {
+        const imgElement = document.createElement('img');
+        imgElement.src = image.previewURL;
+        imageContainer.appendChild(imgElement);
+    });
+}
+
+loadMoreButton.addEventListener('click', async () => {
+    currentPage++;
+    await displayImages();
+});
+
+displayImages();
